ae240828e5557d446e3329e1ce9e2ccfe1ec5253
Cole West <npwest@udel.edu>
2025-11-20 14:55:41 -0500

    added dependencies to .ini file

--- project5_cpeg222_pio/src/main.cpp
+++ project5_cpeg222_pio/src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   int result = myFunction(2, 3);
+  Wire.begin(address);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("CPEG222 Project 5 - BME280 Values");
 }
 
 // put function definitions here:
